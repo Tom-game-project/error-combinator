fe2d1ba63c222bb395d49d61f2873c47058f5cf5
muranaka tomonari <tom.ipynb@gmail.com>
2025-12-20 00:28:35 +0900

    add `and` method to `Check` trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-pub trait Check<T:?Sized> {
+pub trait Check<T:?Sized> 
+    where Self: Sized
+{
     type Error;
 
     fn check(&self, value: &T) -> Result<(), Self::Error>;
+    fn and<A>(self, other: A) -> And<Self, A>
+        where A:Check<T, Error = Self::Error> {
+            And { a: self, b: other }
+    }
 }
 
 impl<T:?Sized, E, F> Check<T> for F
@@ -14,8 +20,31 @@ impl<T:?Sized, E, F> Check<T> for F
     }
 }
 
+// use std::marker::PhantomData;
+
+struct And<A, B> {
+    a: A,
+    b: B,
+}
+
+impl <T:?Sized, A, B, E> Check<T> for And<A, B>
+    where A: Check<T, Error = E>,
+          B: Check<T, Error = E>,
+{
+    type Error = E;
+
+    fn check(&self, value: &T) -> Result<(), Self::Error> {
+        self.a.check(value)?;
+        self.b.check(value)?;
+        Ok(())
+    }
+}
+
+// ------------------------------------
+
 pub enum ValidateErr {
     NotStartsWithHello,
+    NotEnoughtLength,
 }
 
 pub fn check_starts_with_hello(data: &str) -> Result<(), ValidateErr> {
@@ -26,12 +55,20 @@ pub fn check_starts_with_hello(data: &str) -> Result<(), ValidateErr> {
     }
 }
 
+pub fn check_min3(data: &str) -> Result<(), ValidateErr> {
+    if 0 < data.len() {
+        Ok(())
+    } else {
+        Err(ValidateErr::NotEnoughtLength)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works00() {
         {
             let data = "hello world";
             let a = check_starts_with_hello;
@@ -49,6 +86,17 @@ mod tests {
             assert!(result.is_err());
         }
         // println!("Ok!!!");
+    }
+
+    #[test]
+    fn it_works01() {
+        let data = "hello world";
+        let a = 
+            check_starts_with_hello
+            .and(check_min3);
+        let result = a.check(&data);
+
+        assert!(result.is_ok());
     }
 }
 
